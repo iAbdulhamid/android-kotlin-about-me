5c0de8a29f5a7ede8ff933b75c7df77d75014cf8
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2023-05-27 15:22:32 +0200

    Adding a Done Button to Accept Text Input

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.aboutme
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,8 +16,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.aboutme.ui.theme.AboutMeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var nicknameText : EditText
+    private lateinit var doneButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //doneButton.setOnClickListener {
+        //    nicknameText = findViewById<EditText?>(R.id.edit_nickname)
+        //    Toast.makeText(this, "${nicknameText.text}", Toast.LENGTH_SHORT).show()
+        //}
+
     }
 }
